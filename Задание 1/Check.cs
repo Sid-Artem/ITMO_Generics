c92741d894578332aa6ae2e32c2ae8616dfbde19
Сидоров Артем <sidoroffartemii@gmail.com>
2024-08-31 21:24:41 +0300

    Добавьте файлы проекта.

---
+++ Задание 1/Check.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_1
+{
+    internal class Check <T>
+    {
+        private T Number {  get; set; }
+        private double Balance { get; set; }
+        private string FIO { get; set; }
+        //public Check (T number, double balance, string fio)
+        //{
+        //    this.Number = number;
+        //    this.Balance = balance;
+        //    this.FIO = fio;
+        //}
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счета");
+            Number= (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите баланс счета");
+            Balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            FIO = Console.ReadLine();
+
+        }
+        public string Output() 
+        { 
+            return ($"{Number} {Balance} {FIO}");
+        
+        }
+        
+    }
+    
+
+}
---
+++ Задание 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Check<int> check1 = new Check<int>();
+            check1.Input();
+            Console.WriteLine(check1.Output());
+
+            Check<string> check2 = new Check<string>();
+            check2.Input();
+            Console.WriteLine(check2.Output());
+            Console.ReadKey();
+        }
+    }
+}
